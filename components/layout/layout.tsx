915a76f2e0b236ae6a3e169a4aa79b73a1d6c15f
Bartosz Kalota <bkalota@netizens.pl>
2022-01-11 20:57:42 +0100

    layout

---
+++ components/layout/layout.tsx
@@ -0,0 +1,25 @@
+import { Footer } from 'components/footer';
+import { Header } from 'components/header';
+import Head from 'next/head';
+
+import styles from 'styles/layout.module.css';
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+export const AppLayout = ({ children }: IProps) => (
+  <div className={styles.layout}>
+    <Head>
+      <title>Star Wars Navigation System</title>
+      <meta name="description" content="Star Wars Navigation System" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Header />
+
+    {children}
+
+    <Footer />
+  </div>
+);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { AppLayout } from 'components/layout';
+import type { AppProps } from 'next/app';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  );
 }
 
-export default MyApp
+export default App;
