0121195e812990fed136aee831be82a168ebc49e
Bartosz Kalota <bkalota@netizens.pl>
2022-01-11 20:44:37 +0100

    header

---
+++ components/header/header.tsx
@@ -0,0 +1,7 @@
+import styles from 'styles/header.module.css';
+
+export const Header = () => (
+  <header className={styles.header}>
+    <h1 className={styles.title}>Star Wars Navigation System</h1>
+  </header>
+);
