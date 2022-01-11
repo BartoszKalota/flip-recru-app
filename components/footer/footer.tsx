e8154f6fc2b0190d56f56ffa36208237e0e9328d
Bartosz Kalota <bkalota@netizens.pl>
2022-01-11 20:43:42 +0100

    footer

---
+++ components/footer/footer.tsx
@@ -0,0 +1,7 @@
+import styles from 'styles/footer.module.css';
+
+export const Footer = () => (
+  <footer className={styles.footer}>
+    <small className={styles['footer-text']}>Copyright 2022</small>
+  </footer>
+);
