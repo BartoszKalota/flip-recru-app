57f9c5062c2c3fdc453cb715cdc30d9db2b34e4b
Bartosz Kalota <bkalota@netizens.pl>
2022-01-12 19:39:06 +0100

    react-testing-library setup

---
+++ components/footer/__tests__/footer.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+// import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+// import LoginForm, { Props } from "../LoginForm";
+
+describe("<Footer />", () => {
+  test("should display a footer", async () => {
+    // ???
+  });
+});
